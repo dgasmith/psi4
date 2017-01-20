8ea2805a5a7bcd75244b6c648eca84eb0b452d2a
Daniel Smith <malorian@me.com>
2017-01-20 15:40:47 -0500

    XC: CG and DIIS solvers added

--- psi4/src/psi4/dfmp2/mp2.cc
+++ psi4/src/psi4/dfmp2/mp2.cc
@@ -228,7 +228,7 @@ double DFMP2::compute_energy()
 SharedMatrix DFMP2::compute_gradient()
 {
     print_header();
-    
+
     if (Ca_subset("AO","ACTIVE_OCC")->colspi()[0] == 0) {
         if (Cb_subset("AO","ACTIVE_OCC")->colspi()[0] == 0) {
             throw PSIEXCEPTION("There are no occupied orbitals with alpha or beta spin.");
@@ -3016,7 +3016,7 @@ void UDFMP2::print_header()
     int avir_b = Cavir_b_->colspi()[0];
     int occ_b = focc_b + aocc_b;
     int vir_b = fvir_b + avir_b;
-    
+
 
     outfile->Printf( "\t --------------------------------------------------------\n");
     outfile->Printf( "\t                 NBF = %5d, NAUX = %5d\n", basisset_->nbf(), ribasis_->nbf());
@@ -3025,7 +3025,7 @@ void UDFMP2::print_header()
     outfile->Printf( "\t %7s %7d %7d %7d %7d %7d %7d\n", "ALPHA", focc_a, occ_a, aocc_a, avir_a, vir_a, fvir_a);
     outfile->Printf( "\t %7s %7d %7d %7d %7d %7d %7d\n", "BETA", focc_b, occ_b, aocc_b, avir_b, vir_b, fvir_b);
     outfile->Printf( "\t --------------------------------------------------------\n\n");
-    
+
 }
 void UDFMP2::form_Aia()
 {
@@ -3700,7 +3700,7 @@ void RODFMP2::print_header()
     outfile->Printf( "\t %7s %7d %7d %7d %7d %7d %7d\n", "ALPHA", focc_a, occ_a, aocc_a, avir_a, vir_a, fvir_a);
     outfile->Printf( "\t %7s %7d %7d %7d %7d %7d %7d\n", "BETA", focc_b, occ_b, aocc_b, avir_b, vir_b, fvir_b);
     outfile->Printf( "\t --------------------------------------------------------\n\n");
-    
+
 }
 
 }}
